243b969c7a12e6eff9b840618a2d26119393ef00
tirth-kothari <67952030+tirth-kothari@users.noreply.github.com>
2023-10-03 21:45:29 -0400

    Create Solution.py

---
+++ 413. Arithmetic Slices/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0]*(n-1)
+        
+        for i in range(1,n):
+            dp[i-1]=nums[i]-nums[i-1]
+            
+        #print(dp)
+        
+        output = 0
+        m=1
+        
+        for i in range(1,len(dp)):
+            if dp[i]==dp[i-1]:
+                output += m
+                m +=1
+            else:
+                m=1
+        return output
