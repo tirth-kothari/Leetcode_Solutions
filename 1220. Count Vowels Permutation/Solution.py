dcaa493f31e09ddac7abc1bda81a6d35fe6d29e2
tirth-kothari <67952030+tirth-kothari@users.noreply.github.com>
2023-10-27 22:51:17 -0400

    Create Solution.py

---
+++ 1220. Count Vowels Permutation/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        MODULO = 10**9 + 7
+        dp = [1] * 5 # number of string end at character i, with i=[a, e, i, o, u] 
+        for _ in range(1, n):
+            a, e, i, o, u = dp
+            dp[0] = (e + i + u) % MODULO
+            dp[1] = (a + i) % MODULO
+            dp[2] = (e + o) % MODULO
+            dp[3] = i % MODULO
+            dp[4] = (i + o) % MODULO
+            
+        return sum(dp) % MODULO    
